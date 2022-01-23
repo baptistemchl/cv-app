5a5fd2e1981304d61b37f319b967d7e121e63204
baptistemchl <61731553+baptistemchl@users.noreply.github.com>
2022-01-23 12:13:45 +0100

    Delete config.dart

--- lib/utils/config.dart
+++
@@ -1,8 +0,0 @@
-class Config {
-  //https//airtable.com/account
-  static String airtableApikey = "key0WV8JH1c3Pov30";
-  //https//airtable.com/api
-  static String airtableProjectBase = "appzeLamg2Fn80zgH";
-  //Nom de la base
-  static String airtableRecordName = "SDWcv";
-}
